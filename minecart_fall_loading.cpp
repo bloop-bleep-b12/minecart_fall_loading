27267a0ae9b1d492ef9d21fe306b48325a7c362b
bloop-bleep-b12 <45541395+bloop-bleep-b12@users.noreply.github.com>
2020-03-12 22:34:35 -0700

    Add files via upload
    Added support for units being loading into minecarts when they fall from above.
    Currently only one unit at a time may be loaded into a minecart this way.

--- minecart_fall_loading.cpp
+++ minecart_fall_loading.cpp
@@ -1,5 +1,5 @@
 /*
-This is a plugin which allows items that fall on top of minecarts to be loaded into the minecart.
+This is a plugin which allows items or units that fall on top of minecarts to be loaded into the minecart.
 */
 
 #include "Core.h"
@@ -10,8 +10,10 @@ This is a plugin which allows items that fall on top of minecarts to be loaded i
 #include "MiscUtils.h"
 
 #include <vector>
-#include <cmath>
+#include <set>
+#include <algorithm>
 #include <fstream>
+#include <cmath>
 
 #include "df/world.h"
 #include "df/vehicle.h"
@@ -20,12 +22,15 @@ This is a plugin which allows items that fall on top of minecarts to be loaded i
 #include "df/general_ref_contains_itemst.h"
 #include "df/general_ref_contained_in_itemst.h"
 #include "df/general_ref_projectile.h"
+#include "df/general_ref_unit_riderst.h"
 #include "df/item.h"
 #include "df/item_toolst.h"
 #include "df/itemdef_toolst.h"
+#include "df/unit.h"
 
 #include "modules/MapCache.h"
 #include "modules/Items.h"
+#include "modules/Units.h"
 
 using namespace DFHack;
 
@@ -41,7 +46,7 @@ REQUIRE_GLOBAL(world);
 
 // Debugging macros
 
-//#define _DEBUG__MINECART_FALL_LOADING_
+#define _DEBUG__MINECART_FALL_LOADING_
 
 #ifdef _DEBUG__MINECART_FALL_LOADING_
 	// logfile appears in root folder of DF directory
@@ -84,6 +89,10 @@ std::ostream& operator<<(std::ostream& o_st, const std::set<targs...>& s) {
 
 
 
+// general class wrapping things that can be loaded into minecarts; currently items and units
+// this is defined later
+class Loadable;
+
 struct minecart_info {
 	// struct containing info about a minecart for the purposes of this plugin
 	
@@ -95,10 +104,10 @@ struct minecart_info {
 	// next_pos: last recorded predicted next position
 	df::coord next_pos; 
 	
-	// above_pos: last recorded set of items in tile above this->pos
-	std::set<df::item*> above_pos;
-	// above_next_pos: last recorded set of items in tile above this->next_pos
-	std::set<df::item*> above_next_pos;
+	// above_pos: last recorded set of loadables in tile above this->pos
+	std::set<Loadable*> above_pos;
+	// above_next_pos: last recorded set of loadables in tile above this->next_pos
+	std::set<Loadable*> above_next_pos;
 };
 
 typedef df::vehicle::key_field_type minecart_id_t;
@@ -106,6 +115,11 @@ typedef df::vehicle::key_field_type minecart_id_t;
 // minecarts: the info records for all minecarts being tracked
 std::map<minecart_id_t, minecart_info*> minecarts;
 typedef decltype(minecarts)::iterator minecarts_iter_t;
+
+df::item* get_minecart_item(df::vehicle* minecart) {
+	// get the item associated with minecart <minecart>
+	return df::item::find(minecart->item_id);
+}
 
 df::map_block* get_map_block(df::coord pos) {
 	// get the map_block in which coord <pos> is located
@@ -124,6 +138,21 @@ std::set<df::item*> get_items_at(df::coord pos) {
 			out.insert(item);
 		}
 	}
+	
+	return out;
+}
+
+std::set<df::unit*> get_units_at(df::coord pos) {
+	// get the set of units at coord <pos>
+	std::set<df::unit*> out;
+	
+	// loop through all active units
+	for (df::unit* unit : world->units.active) {
+		if (unit->pos == pos) {
+			out.insert(unit);
+		}
+	}
+	
 	return out;
 }
 
@@ -153,15 +182,22 @@ int32_t get_item_loaded_volume(df::item* item) {
 	return out;
 }
 
-bool can_item_fit(df::item* container, df::item* check_fit) {
-	// returns whether item <check_fit> can go inside item <container> without exceeding <container>'s capacity
-	int32_t load_capacity = get_item_load_capacity(container);
-	int32_t loaded_volume = get_item_loaded_volume(container);
+bool can_item_fit(df::vehicle* minecart, df::item* check_fit) {
+	// returns whether item <check_fit> can go inside minecart <minecart> without exceeding <minecart>'s capacity
+	df::item* minecart_item = get_minecart_item(minecart);
+	int32_t load_capacity = get_item_load_capacity(minecart_item);
+	int32_t loaded_volume = get_item_loaded_volume(minecart_item);
 	int32_t check_fit_volume = check_fit->getVolume();
 	DEBUG_PRINTLN_EXPR(load_capacity);
 	DEBUG_PRINTLN_EXPR(loaded_volume);
 	DEBUG_PRINTLN_EXPR(check_fit_volume);
 	return loaded_volume + check_fit_volume <= load_capacity;
+}
+
+bool can_unit_fit(df::vehicle* minecart, df::unit* check_fit) {
+	// returns whether unit <check_fit> can go inside minecart <minecart>
+	// currently this is whether there is not already a unit inside <minecart>
+	return !(get_minecart_item(minecart))->flags2.bits.has_rider;
 }
 
 void make_not_projectile(df::item* item) {
@@ -303,7 +339,7 @@ void load_minecart_with_item(df::vehicle* minecart, df::item* item) {
 	DEBUG_PRINTLN("finished removing shelved_refs");
 	
 	MapExtras::MapCache mc;
-	bool did_succeed = Items::moveToContainer(mc, item, df::item::find(minecart->item_id));
+	bool did_succeed = Items::moveToContainer(mc, item, get_minecart_item(minecart));
 	DEBUG_PRINTLN_EXPR(did_succeed);
 	
 	// restoration of shelved_refs
@@ -312,6 +348,24 @@ void load_minecart_with_item(df::vehicle* minecart, df::item* item) {
 	}
 	
 	DEBUG_PRINTLN("finished putting back shelved_refs");
+}
+
+void load_minecart_with_unit(df::vehicle* minecart, df::unit* unit) {
+	// load minecart <minecart> with unit <unit>
+	DEBUG_PRINTLN("load_minecart_with_unit");
+	df::item* minecart_item = get_minecart_item(minecart);
+	
+	// change minecart
+	auto gen_ref = df::allocate<df::general_ref_unit_riderst>();
+	gen_ref->unit_id = unit->id;
+	minecart_item->general_refs.push_back(gen_ref);
+	minecart_item->flags2.bits.has_rider = true;
+	
+	// change unit
+	unit->mount_type = 0;
+	unit->riding_item_id = minecart_item->id;
+	unit->flags1.bits.rider = true;
+	//unit->flags3.bits.exit_vehicle1 = true;
 }
 
 int div_floor(int dividend, int divisor) {
@@ -330,12 +384,120 @@ df::coord get_next_pos(df::vehicle* minecart, df::coord current_pos) {
 	);
 }
 
-minecart_info* create_new_minecart_info(df::vehicle* v) {
-	// returns a pointer to a new info record for minecart <v>
+
+
+class Loadable {
+	// general class wrapping things that can be loaded into minecarts; currently items and units
+	// abstract, subclassed to provide implementation for each specific case
+	public:
+		// constructor
+		Loadable();
+		virtual ~Loadable();
+		
+		// returns position of this in active map
+		virtual df::coord pos() const = 0;
+		// returns whether this can fit into given vehicle
+		virtual bool can_fit(df::vehicle*) const = 0;
+		// loads this into given vehicle
+		virtual void load(df::vehicle*) = 0;
+};
+
+class ItemLoadable : public Loadable {
+	// Loadable that wraps items
+	private:
+		df::item* contents;
+	public:
+		ItemLoadable(df::item*);
+		
+		virtual df::coord pos() const;
+		virtual bool can_fit(df::vehicle*) const;
+		virtual void load(df::vehicle*);
+};
+
+class UnitLoadable : public Loadable {
+	// Loadable that wraps units
+	private:
+		df::unit* contents;
+	public:
+		UnitLoadable(df::unit*);
+		
+		virtual df::coord pos() const;
+		virtual bool can_fit(df::vehicle*) const;
+		virtual void load(df::vehicle*);
+};
+
+Loadable::Loadable() {}
+
+Loadable::~Loadable() {}
+
+// implementation functions of Loadable subclasses delegate to global functions
+
+ItemLoadable::ItemLoadable(df::item* i_contents)
+  : Loadable(),
+    contents(i_contents)
+{}
+
+df::coord ItemLoadable::pos() const {
+	return Items::getPosition(contents);
+}
+
+bool ItemLoadable::can_fit(df::vehicle* minecart) const {
+	return can_item_fit(minecart, contents);
+}
+
+void ItemLoadable::load(df::vehicle* minecart) {
+	load_minecart_with_item(minecart, contents);
+}
+
+UnitLoadable::UnitLoadable(df::unit* i_contents)
+  : Loadable(),
+    contents(i_contents)
+{}
+
+df::coord UnitLoadable::pos() const {
+	return Units::getPosition(contents);
+}
+
+bool UnitLoadable::can_fit(df::vehicle* minecart) const {
+	return can_unit_fit(minecart, contents);
+}
+
+void UnitLoadable::load(df::vehicle* minecart) {
+	load_minecart_with_unit(minecart, contents);
+}
+
+
+
+std::set<Loadable*> get_loadables_at(df::coord pos) {
+	// returns the set of all loadables at position <pos>
+	
+	std::set<Loadable*> out;
+	std::set<df::item*> item_set = get_items_at(pos);
+	std::set<df::unit*> unit_set = get_units_at(pos);
+	
+	// wrap and insert each item
+	std::transform(
+		item_set.begin(),
+		item_set.end(),
+		std::inserter(out, out.begin()),
+		[](df::item* item) -> Loadable* { return new ItemLoadable(item); }
+	);
+	// wrap and insert each unit
+	std::transform(
+		unit_set.begin(),
+		unit_set.end(),
+		std::inserter(out, out.begin()),
+		[](df::unit* unit) -> Loadable* { return new UnitLoadable(unit); }
+	);
+	return out;
+}
+
+minecart_info* create_new_minecart_info(df::vehicle* minecart) {
+	// returns a pointer to a new info record for minecart <minecart>
 	// will be properly initialized later in the update, during the call to update_minecart_info
 	minecart_info* out = new minecart_info;
-	out->minecart = v;
-	out->minecart_item = df::item::find(v->item_id);
+	out->minecart = minecart;
+	out->minecart_item = get_minecart_item(minecart);
 	out->pos = df::coord();
 	out->next_pos = df::coord();
 	out->above_pos = {};
@@ -358,6 +520,7 @@ void update_minecart_list() {
 	
 	std::vector<minecarts_iter_t> to_remove;
 	for (auto iter = minecarts.begin(); iter != minecarts.end(); ++iter) {
+		// if id is no longer found
 		if (df::vehicle::find(iter->first) == nullptr) {
 			to_remove.push_back(iter);
 		}
@@ -372,10 +535,12 @@ void update_minecart_list() {
 	
 	unsigned int num_inserted = 0;
 	for (df::vehicle* v : world->vehicles.all) {
+		// this call will not perform an insertion if an equivalent key already exists
 		auto pr = minecarts.insert(std::make_pair(
 			v->id,
 			create_new_minecart_info(v)
 		));
+		// pr.second is true iff an insertion was made
 		if (pr.second) {
 			++num_inserted;
 		}
@@ -403,9 +568,9 @@ void perform_minecart_loading() {
 		
 		DEBUG_PRINTLN_EXPR(current_pos);
 		
-		// above_set: the set of items that was *last recorded* being above the minecart's *current* position
+		// above_set: the set of loadables that was *last recorded* being above the minecart's *current* position
 		// this is above_pos if the minecart hasn't moved since the last update, and above_next_pos if it has moved
-		std::set<df::item*>& above_set = (info->pos == current_pos) ? info->above_pos : info->above_next_pos;
+		std::set<Loadable*>& above_set = (info->pos == current_pos) ? info->above_pos : info->above_next_pos;
 		
 		DEBUG_PRINTLN_EXPR(above_set);
 		
@@ -413,21 +578,33 @@ void perform_minecart_loading() {
 			DEBUG_PRINTLN("perform_minecart_loading: minecart INTEREST 1");
 		}
 		
-		for (df::item* item : above_set) {
-			DEBUG_PRINTLN_EXPR(item->id);
+		for (Loadable* loadable : above_set) {
+			DEBUG_PRINTLN_EXPR(loadable);
 			// if item has moved onto the minecart's current position since the last update
-			if (Items::getPosition(item) == current_pos) {
-				DEBUG_PRINTLN("item fell onto minecart");
+			if (loadable->pos() == current_pos) {
+				DEBUG_PRINTLN("loadable fell onto minecart");
 				// if the item can fit in the minecart
-				if (can_item_fit(minecart_item, item)) {
-					DEBUG_PRINTLN("item can fit");
-					DEBUG_PRINTLN("item to be loaded");
+				if (loadable->can_fit(minecart)) {
+					DEBUG_PRINTLN("loadable can fit");
+					DEBUG_PRINTLN("loadable to be loaded");
 					// load the minecart with the item
-					load_minecart_with_item(minecart, item);
+					loadable->load(minecart);
 				}
 			}
 		}
 	}
+}
+
+template <typename ContainerT>
+void delete_each_of(ContainerT& container) {
+	// deletes each pointer in container <container>, then clears it
+	// <container> of course must be of dynamically allocated pointers
+	std::for_each(
+		container.begin(),
+		container.end(),
+		[](ContainerT::value_type ptr) { delete ptr; }
+	);
+	container.clear();
 }
 
 void update_minecart_info() {
@@ -441,7 +618,7 @@ void update_minecart_info() {
 		DEBUG_PRINTLN_EXPR(id);
 		
 		df::vehicle* minecart = df::vehicle::find(id);
-		df::item* minecart_item = df::item::find(minecart->item_id);
+		df::item* minecart_item = get_minecart_item(minecart);
 		df::coord current_pos = Items::getPosition(minecart_item);
 		
 		DEBUG_PRINTLN_EXPR(current_pos);
@@ -451,9 +628,12 @@ void update_minecart_info() {
 		
 		DEBUG_PRINTLN_EXPR(info->next_pos);
 		
-		info->above_pos = get_items_at(info->pos + df::coord(0, 0, 1));
+		delete_each_of(info->above_pos);
+		delete_each_of(info->above_next_pos);
+		
+		info->above_pos = get_loadables_at(info->pos + df::coord(0, 0, 1));
 		DEBUG_PRINTLN_EXPR(info->above_pos);
-		info->above_next_pos = get_items_at(info->next_pos + df::coord(0, 0, 1));
+		info->above_next_pos = get_loadables_at(info->next_pos + df::coord(0, 0, 1));
 		DEBUG_PRINTLN_EXPR(info->above_next_pos);
 		
 		if (info->above_pos.size() != 0) {
